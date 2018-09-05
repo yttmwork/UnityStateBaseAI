d83cf5329a8efed7583811669c0379ba7abbf6fb
DESKTOP-EI8V3J2\yotto <yttmwork@gmail.com>
2018-09-06 08:39:18 +0900

    【<task> #7】IStateインターフェースを使えるようにするためにMosterクラスを修正

--- Assets/Script/Monster.cs
+++ Assets/Script/Monster.cs
@@ -3,14 +3,63 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Monster : MonoBehaviour {
+    
+    // 状態用定数
+    public enum State
+    {
+        WAIT,           // 待機
+        WALK,           // 移動
+        CHASE,          // 追跡
+        ATTACK,         // 攻撃
+        ATTACK_WAIT,    // 攻撃待機
+        DAMAGE,         // ダメージ
+        DIE,            // 死亡
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField]
+    private State activeState;          // 今の状態
+
+    private IStateBase state;           // 状態インスタンス
+
+    // 状態切り替え
+    public void ChangeState(IStateBase next_state)
+    {
+        this.state = next_state;
+        this.state.Init(this);
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        switch (this.activeState)
+        {
+            case State.WAIT:
+
+                break;
+            case State.WALK:
+
+                break;
+            case State.CHASE:
+
+                break;
+            case State.ATTACK:
+
+                break;
+            case State.ATTACK_WAIT:
+
+                break;
+            case State.DAMAGE:
+
+                break;
+            case State.DIE:
+
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        this.state.Update(this);
+    }
 }
