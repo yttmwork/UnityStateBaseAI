fae04c80ab57771a738596944782c27770413633
DESKTOP-EI8V3J2\yotto <yttmwork@gmail.com>
2018-09-06 08:35:43 +0900

    【<task> #7】StateBaseの規定となるインターフェースの作成

---
+++ Assets/Script/IStateBase.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IStateBase
+{
+    // 初期化
+    void Init(Monster monster);
+
+    // 更新
+    void Update(Monster monster);
+
+}
