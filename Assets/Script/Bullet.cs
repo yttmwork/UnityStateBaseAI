830e17e05c4e1c9b6b7130b43bbdfda934f6f1c8
DESKTOP-EI8V3J2\yotto <yttmwork@gmail.com>
2018-09-06 09:12:05 +0900

    【<task> #7】ダメージ処理のチェックをするためのプレイヤーと攻撃の実装

---
+++ Assets/Script/Bullet.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+    [SerializeField]
+    private float speed;        // 速度
+
+    [SerializeField]
+    private float lifeTime;     // 寿命
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Translate(new Vector3(0.0f, 0.0f, this.speed) * Time.deltaTime);
+
+        // 寿命を減らす
+        this.lifeTime -= Time.deltaTime;
+        if (this.lifeTime <= 0.0f)
+        {
+            // 削除
+            Destroy(this.gameObject);
+        }
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // 弾が敵に当たれば消す
+        if (other.tag == "Enemy")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Script/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField]
+    GameObject bulletPrefab;    // 弾プレハブ
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Zで発射
+		if (Input.GetKeyDown(KeyCode.Z))
+        {
+            GameObject obj = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+            // プレイヤーの向きに回転
+            obj.transform.Rotate(transform.eulerAngles);
+        }
+	}
+}
