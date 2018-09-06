ec5e35c380ee96a29f32ad4afa0bd84804441558
DESKTOP-EI8V3J2\yotto <yttmwork@gmail.com>
2018-09-06 09:05:30 +0900

    【<task> #7】攻撃状態のフロー実装 切り替えはアニメーションありきなので切り替え処理は仮 遷移処理も未実装

---
+++ Assets/Script/AttackState.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackState : IStateBase
+{
+    private static AttackState instance = new AttackState();
+    public static AttackState Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    public void Init(Monster monster)
+    {
+        // debug アニメーション実装までは時間で管理
+        monster.Timer = 0.0f;
+    }
+
+    public void Update(Monster monster)
+    {
+        // debug アニメーション実装までは時間で管理
+        monster.Timer += Time.deltaTime;
+
+        // アニメーション終了で切り替え
+        // debug アニメーション実装までは時間で管理
+        if (monster.Timer > 2.0f)
+        {
+            Debug.Log("攻撃待機に切り替え");
+        }
+    }
+}
