871d2b86713407843e0f44ab4efa04cada54b1b3
DESKTOP-EI8V3J2\yotto <yttmwork@gmail.com>
2018-09-06 09:27:12 +0900

    【<task> #7】ダメージ状態のフロー実装 遷移にはアニメーション切り替え処理がいるのでそれらの部分は仮 遷移処理も未実装

---
+++ Assets/Script/DamageState.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageState : IStateBase
+{
+    private static DamageState instance = new DamageState();
+    public static DamageState Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    public void Init(Monster monster)
+    {
+        // debug アニメーション実装までは時間で管理
+        monster.Timer = 0.0f;
+    }
+
+    public void Update(Monster monster)
+    {
+        // debug アニメーション実装までは時間で管理
+        monster.Timer += Time.deltaTime;
+
+        // debug アニメーション実装までは時間で管理
+        if (monster.Timer > 2.0f)
+        {
+            Debug.Log("待機に切り替え");
+        }
+    }
+}
